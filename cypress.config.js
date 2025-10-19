5c706580714cddc6cb77ce0be99f1b7ad1b222ef
Mykhail <its.astral.step@gmail.com>
2025-10-19 22:18:01 +0300

    preprocessor configs

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,6 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
-async function setupNodeEvents(on, config) {
-    // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-    await preprocessor.addCucumberPreprocessorPlugin(on, config);
-    on(
-        "file:preprocessor",
-        createBundler({
-            plugins: [createEsbuildPlugin.default(config)],
-        })
-    );
-    // Make sure to return the config object as it might have been modified by the plugin.
-    return config;
-}
 
 module.exports = defineConfig({
     defaultCommandTimeout: 10000,
@@ -27,8 +15,17 @@ module.exports = defineConfig({
         baseUrl: "https://telnyx.com",
         specPattern: "**/*.feature",
         supportFile: false,
-        setupNodeEvents(on, config) {
+        async setupNodeEvents(on, config) {
             require('cypress-mochawesome-reporter/plugin')(on)
+            await preprocessor.addCucumberPreprocessorPlugin(on, config)
+            on(
+                "file:preprocessor",
+                createBundler({
+                    plugins: [createEsbuildPlugin.default(config)],
+                })
+            );
+            // Make sure to return the config object as it might have been modified by the plugin.
+            return config;
         }
     },
     reporter: 'cypress-mochawesome-reporter',
